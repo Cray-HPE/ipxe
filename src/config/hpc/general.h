d206127cfc66f83c1460163348f37c03be2fc7e1
Russell Bunch <doomslayer@hpe.com>
2022-09-09 11:41:56 -0500

    [hpc] do not try to override branding

--- src/config/hpc/general.h
+++ src/config/hpc/general.h
@@ -1,8 +1,3 @@
-#undef PRODUCT_NAME
-#define PRODUCT_NAME "HPC Metal iPXE"
-#undef PRODUCT_URI
-#define PRODUCT_URI "https://github.com/Cray-HPE/ipxe"
-
 /* Enable VLAN command: https://ipxe.org/buildcfg/vlan_cmd */
 #define VLAN_CMD
 
